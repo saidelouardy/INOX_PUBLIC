0531f0961fdd10d24dba3da88e9a051327fe7ee2
saidelouardy <saidelouardy609@gmail.com>
2023-05-16 13:44:50 +0100

    I finish the description of the company page

--- app/src/main/java/com/example/inoxpublic/sixgooglemaps/MainActivityGooglemaps.java
+++ app/src/main/java/com/example/inoxpublic/sixgooglemaps/MainActivityGooglemaps.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivityFore extends AppCompatActivity {
+public class MainActivityGooglemaps extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_fore);
+        setContentView(R.layout.activity_main_googlemaps);
     }
 }
